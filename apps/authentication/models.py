52b07502a6f01173f8f0a29b765e4072e8c5d1db
johnalexguerrero <johnalexguerrero@gmail.com>
2024-04-05 00:29:15 -0500

    first commit

---
+++ apps/authentication/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True)
+    second_name = models.TextField(max_length=50, blank=True, null=True)
+    
+    #campo identificador unico del User
+    USERNAME_FIELD = 'email'
+    
+    #forzar a introducir un campo
+    REQUIRED_FIELDS = ['username','password']

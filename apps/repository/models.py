acd84bd4f3e59efd5bf8a805817ec36cd137cf11
johnalexguerrero <johnalexguerrero@gmail.com>
2024-04-08 19:55:07 -0500

    repository: models(requeriments, fucntionalities,etc)

--- apps/repository/models.py
+++ apps/repository/models.py
@@ -15,6 +15,17 @@ class License(models.IntegerChoices):
 class TypeOfWork(models.IntegerChoices):
     UNPUBLISHED = 1, "Obra inédita"
     DERIVED = 2, "Obra derivada"
+
+class TypeFunctionSoftware(models.IntegerChoices):
+    APPLICATION = 1, "Software de aplicación"
+    MANAGEMENT = 2, "Software de gestión"
+    PROGRAMMING = 3, "Software de desarrollo"
+    SYSTEM = 4, "Software de sistema"
+
+class TypePublic(models.IntegerChoices):
+    ALL_PUBLIC = 1, "Apto para todo público"
+    TEENAGERS = 2, "Adolecentes"
+    ADULT = 3, "Mayores de 17 años" 
     
 class Software(models.Model):
     title = models.CharField(("Software titulo"), max_length=150, null=False, unique=True)
@@ -43,3 +54,54 @@ class Software(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+                            
+class Category(models.Model):
+    software = models.OneToOneField(Software, verbose_name=("software"), on_delete=models.CASCADE)
+    type_software = models.PositiveSmallIntegerField(("Tipo de software por funcionalidad"), choices=TypeFunctionSoftware.choices, default=TypeFunctionSoftware.APPLICATION)    
+    type_public = models.PositiveSmallIntegerField(("Tipo de audiencia"), choices=TypePublic.choices)
+    collection = models.CharField(("categoria"), max_length=250, blank=True, null=True)
+    os = models.CharField(("sistema operativo"), max_length=250, blank=False, null=False)
+    
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+
+    def __str__(self):
+        return self.software.title
+
+    def get_absolute_url(self):
+        return reverse("Category_detail", kwargs={"pk": self.pk})
+
+class Requeriment(models.Model):
+    software = models.ForeignKey(Software, verbose_name=("software"), on_delete=models.CASCADE)
+    description = models.CharField(("descripción"), max_length=250, null=False, blank=False)
+    version = models.CharField(("versión"), max_length=50, null=False)
+    priority = models.PositiveBigIntegerField(("prioridad"), max_length=50, choices=[(1,"Alta"),(2,"Media"),(3,"Baja")])
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    
+
+    class Meta:
+        verbose_name = ("Requeriment")
+        verbose_name_plural = ("Requeriments")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Requeriments_detail", kwargs={"pk": self.pk})
+
+class Funtionality(models.Model):
+    requeriment = models.ForeignKey(Requeriment, verbose_name=("requeriment"), on_delete=models.CASCADE)
+    description = models.CharField(("description"), max_length=150, null=False)
+
+    class Meta:
+        verbose_name = ("Funtionality")
+        verbose_name_plural = ("Funtionalities")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Funtionality_detail", kwargs={"pk": self.pk})
+
